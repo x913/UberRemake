77fc18a08d7cd69cc0014023b5c4d026e38ad43d
x913 <x913x913@gmail.com>
2021-01-18 21:47:23 +0500

    add firebase auth

--- app/src/main/java/com/k3kc/uberremake/SplashScreenActivity.kt
+++ app/src/main/java/com/k3kc/uberremake/SplashScreenActivity.kt
@@ -1,10 +1,14 @@
 package com.k3kc.uberremake
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -45,6 +49,18 @@ class SplashScreenActivity : AppCompatActivity() {
         init()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == LOGIN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+            } else {
+                Toast.makeText(this@SplashScreenActivity, "error " + response!!.error!!.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
     private fun init() {
         providers = Arrays.asList(
             AuthUI.IdpConfig.PhoneBuilder().build(),
@@ -56,8 +72,9 @@ class SplashScreenActivity : AppCompatActivity() {
             val user = it.currentUser
             if(user != null)
                 Toast.makeText(this@SplashScreenActivity, "Welcome: " + user.uid, Toast.LENGTH_SHORT).show()
-            else
+            else {
                 showLoginLayout()
+            }
         }
 
     }
@@ -75,11 +92,8 @@ class SplashScreenActivity : AppCompatActivity() {
                 .setTheme(R.style.LoginTheme)
                 .setAvailableProviders(providers)
                 .setIsSmartLockEnabled(false)
-                .enableAnonymousUsersAutoUpgrade()
                 .build(),
             LOGIN_REQUEST_CODE,
-
         )
-
     }
 }
