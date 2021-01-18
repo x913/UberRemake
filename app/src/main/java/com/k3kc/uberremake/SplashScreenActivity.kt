ec584ad2b9267ed6e9b7fcd1ce31c44a1dd5b714
x913 <x913x913@gmail.com>
2021-01-18 16:39:47 +0500

    Initial commit

---
+++ app/src/main/java/com/k3kc/uberremake/SplashScreenActivity.kt
@@ -0,0 +1,10 @@
+package com.k3kc.uberremake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
