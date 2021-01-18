f6537b01e2b62fe9bfa75c72ad8934842819d94f
x913 <x913x913@gmail.com>
2021-01-18 17:09:36 +0500

    add splash screen

--- app/src/main/java/com/k3kc/uberremake/SplashScreenActivity.kt
+++ app/src/main/java/com/k3kc/uberremake/SplashScreenActivity.kt
@@ -2,9 +2,17 @@ package com.k3kc.uberremake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+            .subscribe({
+                Toast.makeText(this@SplashScreenActivity, "Splash screen done!", Toast.LENGTH_SHORT).show()
+            })
     }
 }
